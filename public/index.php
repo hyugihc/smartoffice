28b6ed11e882ac9168b06f4acfd568da205e042b
Yoga Wira Pratama <hyugihc@gmail.com>
2016-10-25 17:27:11 +0800

    Update to laravel 5.3.19 & add appzcoder -crud generator
    Update to laravel 5.3.19 & add appzcoder -crud generator for ease use

--- public/index.php
+++ public/index.php
@@ -45,7 +45,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
 |
-*/
+*/                          
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
